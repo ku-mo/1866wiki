637d461242544813891c2fb9b8feea636752538e
ku-mo <ku-mo>
2025-11-23 00:10:08 -0500

    fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://https://1866.wiki/',
+  url: 'https://1866.wiki/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
